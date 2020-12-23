de89319c700e79633402b59636a7aa4eae8bd024
kiborroq <kiborroq@student.21-school.ru>
2020-12-23 10:36:19 +0300

    second

--- main.c
+++ main.c
@@ -3,53 +3,96 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kiborroq <kiborroq@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kiborroq@student.42.fr <kiborroq>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/07 12:39:08 by kiborroq          #+#    #+#             */
-/*   Updated: 2020/12/11 10:59:11 by kiborroq         ###   ########.fr       */
+/*   Updated: 2020/12/21 23:07:29 by kiborroq@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <mlx.h>
+#include "minilibx/mlx.h"
 #include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-#define WIDTH 1920
-#define HEIGHT 1080 
+typedef struct  s_data {
+    void        *img;
+    char        *addr;
+    int         bits_per_pixel;
+    int         line_length;
+    int         endian;
+}               t_data;
 
-int main(int argc, char *argv[])
+void            my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
-    int width[1];
-    int height[1];
+    char    *dst;
 
-    width[0] = 64;
-    height[0] = 64;
-    void *new_mlx = mlx_init();
-    void *new_win = mlx_new_window(new_mlx, WIDTH, HEIGHT, "Test");
-    void *new_img = mlx_xpm_file_to_image(new_mlx, argv[1], width, height);
-    if (!new_img)
-    {
-        write(1, "ERROR\n", 6);
-        exit(0);
-    }
-    int x;
-    int y = 0;
-
-    while (y < HEIGHT + 63)
-    {
-        x = 0;
-        while (x < WIDTH + 63)
-        {
-            mlx_put_image_to_window(new_mlx, new_win, new_img, x, y);
-            x += 64;
-        }
-        y += 64;
-    }
-    mlx_loop(new_mlx);
-    free(new_win);
-    free(new_mlx);
-    mlx_destroy_image(new_mlx, new_img);
-    mlx_clear_window(new_mlx, new_win);
-    mlx_destroy_window(new_mlx, new_win);
-    return 0;
+    dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+    *(unsigned int*)dst = color;
 }
+
+int             main(void)
+{
+    void    *mlx;
+    void    *mlx_win;
+    t_data  img;
+
+    mlx = mlx_init();
+    mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+    img.img = mlx_new_image(mlx, 1920, 1080);
+    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+                                 &img.endian);
+    for (int i = 200; i < 1720; i++) {
+        for (int j = 200; j < 880; j++) {
+            my_mlx_pixel_put(&img, i, j, 0x00FF0000);
+        }
+    }
+    mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+    mlx_loop(mlx);
+}
+
+// #define WIDTH 1920
+// #define HEIGHT 1080 
+
+// int main(int argc, char *argv[])
+// {
+//     int width[1];
+//     int height[1];
+
+//     width[0] = 64;
+//     height[0] = 64;
+//     void *new_mlx = mlx_init();
+//     void *new_win = mlx_new_window(new_mlx, WIDTH, HEIGHT, "Test");
+//     void *new_img = mlx_xpm_file_to_image(new_mlx, argv[1], width, height);
+//     if (!new_img)
+//     {
+//         write(1, "ERROR\n", 6);
+//         exit(0);
+//     }
+//     int x;
+//     int y = 0;
+
+
+
+//     while (y < HEIGHT + 63)
+//     {
+//         x = 0;
+//         while (x < WIDTH + 63)
+//         {
+//             mlx_put_image_to_window(new_mlx, new_win, new_img, x, y);
+//             x += 64;
+//         }
+//         y += 64;
+//     }
+
+//     printf("%s\n", mlx_get_data_addr(new_img, 0, 0, 0));
+
+//     mlx_loop(new_mlx);
+//     mlx_destroy_image(new_mlx, new_img);
+//     mlx_clear_window(new_mlx, new_win);
+//     mlx_destroy_window(new_mlx, new_win);
+//     return 0;
+// }
+
+
+// gcc -Werror -Wextra -Wall main.c -L minilibx/ -lmlx -lm -lbsd -lX11 -lXext
--- test.c
+++ test.c
@@ -3,158 +3,427 @@
 /*                                                        :::      ::::::::   */
 /*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kiborroq <kiborroq@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kiborroq@student.42.fr <kiborroq>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/11 10:59:31 by kiborroq          #+#    #+#             */
-/*   Updated: 2020/12/12 13:19:21 by kiborroq         ###   ########.fr       */
+/*   Updated: 2020/12/23 01:18:43 by kiborroq@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #define WIDTH 800
 #define HEIGHT 600
+#define MAP_HEIGHT 10
+#define MAP_WIDTH 10
 
-#include <math.h>
+#define EXIT 65307
+#define MOVE_FORWARD 119
+#define MOVE_BACKWARD 115
+#define MOVE_LEFT 97
+#define MOVE_RIGHT 100
+#define TURN_LEFT 65361
+#define TURN_RIGHT 65363 
 
-typedef struct  s_point
+#define MOVE_SPEED 0.10
+#define TURN_SPEED 0.10
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "unistd.h"
+#include "math.h"
+#include "minilibx/mlx.h"
+
+typedef struct  s_point_double
 {
     double x;
     double y;
-}               t_point;
+}               t_point_double;
 
-typedef struct  s_view
+typedef struct  s_point_int
 {
-    t_point pos;
-    t_point dir;
-    t_point cam_plane;
-}               t_view;
+    int x;
+    int y;
+}               t_point_int;
 
-typedef struct s_ray
+typedef struct      s_view
 {
-    t_point dir;
-    t_point pos;
-    t_point cur_dist;
-    t_point delt_dist;
-    t_point ratio;
-    double dist;
-}               t_ray;
+    t_point_double  pos;
+    t_point_double  dir;
+    t_point_double  cam_plane;
+}                   t_view;
 
-int main(void)
+typedef struct s_turn_sin_cos
 {
-    int	map[10][10]=
-    {
-        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    };
+    double sin_pozotive;
+    double sin_negative;
+    double cos_pozitive;
+    double cos_negative;
+}               t_turn_sin_cos;
 
-    t_view view;
-    view.dir.x = -1;
-    view.dir.y = 0;
-    view.pos.x = 5;
-    view.pos.y = 5;
-    view.cam_plane.x = 0;
-    view.cam_plane.y = 0.66;
+typedef struct s_event
+{
+    int move_forawrd;
+    int move_backword;
+    int move_left;
+    int move_right;
+    int turn_left;
+    int turn_right;
+}               t_event;
 
-    t_ray ray;
-    int x_on_camera_plane;
-    x_on_camera_plane = 0;
-    double pos_on_plane;
+
+typedef struct      s_ray
+{
+    t_point_double  dir;
+    t_point_int     pos;
+    t_point_double  cur_dist;
+    t_point_double  delt_dist;
+    double          dist;
+}                   t_ray;
+
+typedef struct			s_image
+{
+	int					size_line;
+	int					bpp;
+	int					endian;
+	void				*img_ptr;
+	char				*data;
+}						t_image;
+
+typedef struct  s_inf
+{
+    t_view          view;
+    t_ray           ray;
+    t_turn_sin_cos  turn;
+    void            *mlx_ptr;
+    void            *win_ptr;
+    t_image         img;
+    int             **map;
+    t_event          event;
+}                   t_inf;
+
+void    my_mlx_pixel_put(t_image *img, int x, int y, int color)
+{
+    char    *dst;
+
+    dst = img->data + (y * img->size_line + x * (img->bpp / 8));
+    *(unsigned int*)dst = color;
+}
+
+int    raycast(t_inf *inf)
+{
+    inf->img.img_ptr = mlx_new_image(inf->mlx_ptr, WIDTH, HEIGHT);
+    inf->img.data = mlx_get_data_addr( inf->img.img_ptr,
+                                        &inf->img.bpp,
+                                        &inf->img.size_line,
+                                        &inf->img.endian);
+    int x_on_camera_plane = 0;
+    double pos_on_plane = 0;
     while (x_on_camera_plane < WIDTH)
     {
         //set current ray (direction, position, ratio)
-        pos_on_plane = 2 * d / double(WIDTH) - 1;
-        ray.dir.x = view.dir.x * pos_on_plane + view.cam_plane.x;
-        ray.dir.y = view.dir.y * pos_on_plane + view.cam_plane.y;
-        ray.pos.x = int(view.pos.x);
-        ray.pos.y = int(view.pos.y);
-        if (ray.dir.y != 0)
-            ray.ratio.x = ray.dir.x == 0 ? 1 : 1 / ray.dir.x;
+        pos_on_plane = 2 * x_on_camera_plane / (double)WIDTH - 1;
+        inf->ray.dir.x = inf->view.dir.x + pos_on_plane * inf->view.cam_plane.x;
+        inf->ray.dir.y = inf->view.dir.y + pos_on_plane * inf->view.cam_plane.y;
+        inf->ray.pos.x = (int)inf->view.pos.x;
+        inf->ray.pos.y = (int)inf->view.pos.y;
+
+        if (inf->ray.dir.y != 0)
+            inf->ray.delt_dist.x = inf->ray.dir.x == 0 ? 1 : fabs(1 / inf->ray.dir.x);
         else
-            ray.ratio.x = 0;
-        if (ray.dir.x != 0)
-            ray.ratio.y = ray.dir.y == 0 ? 1 : 1 / ray.dir.y;
+            inf->ray.delt_dist.x = 0;
+        if (inf->ray.dir.x != 0)
+            inf->ray.delt_dist.y = inf->ray.dir.y == 0 ? 1 : fabs(1 / inf->ray.dir.y);
         else
-            ray.ratio.y = 0;
+            inf->ray.delt_dist.y = 0;
     
         //Set step for field iterator, initial distance and delta distance for x and y
-        t_point step;
-        if (ray.dir.x > 0)
-        {
-            step.x = 1;
-            ray.cur_dist.x = (view.pos.x - ray.pos.x) * ray.ratio.x;
-        }
-        else
+        t_point_int step;
+        if (inf->ray.dir.x < 0)
         {
             step.x = -1;
-            ray.cur_dist.x = (view.pos.x + 1.0 - ray.pos.x) * ray.ratio.x;
-        }
-        if (ray.dir.y > 0)
-        {
-            step.y = 1;
-            ray.cur_dist.y = (view.pos.y - ray.pos.y) *  ray.ratio.y;
+            inf->ray.cur_dist.x = (inf->view.pos.x - inf->ray.pos.x) * inf->ray.delt_dist.x;
         }
         else
         {
+            step.x = 1;
+            inf->ray.cur_dist.x = (inf->ray.pos.x + 1.0 - inf->view.pos.x) * inf->ray.delt_dist.x;
+        }
+        if (inf->ray.dir.y < 0)
+        {
             step.y = -1;
-            ray.cur_dist.y = (view.pos.y + 1.0 - ray.pos.y) *  ray.ratio.y;
+            inf->ray.cur_dist.y = (inf->view.pos.y - inf->ray.pos.y) *  inf->ray.delt_dist.y;
+        }
+        else
+        {
+            step.y = 1;
+            inf->ray.cur_dist.y = (inf->ray.pos.y + 1.0 - inf->view.pos.y) *  inf->ray.delt_dist.y;
         }
     
         //Ray casting for current ray
-        int hit;
+        int hit = 0;
         int hit_side;
-        hit = 0;
         while (hit == 0)
         {
-            if (ray.cur_dist.x < ray.cur_dist.y)
+            if (inf->ray.cur_dist.x < inf->ray.cur_dist.y)
             {
-                ray.cur_dist.x += ray.delt_dist.x;
-                ray.pos.x += step.x;
+                inf->ray.cur_dist.x += inf->ray.delt_dist.x;
+                inf->ray.pos.x += step.x;
                 hit_side = 1;
             }
             else
             {
-                ray.cur_dist.y += ray.delt_dist.y;
-                ray.pos.y += step.y;
+                inf->ray.cur_dist.y += inf->ray.delt_dist.y;
+                inf->ray.pos.y += step.y;
                 hit_side = 0;
             }
-            if (map[ray.pos.y][ray.pos.x])
+            if (inf->map[inf->ray.pos.y][inf->ray.pos.x])
                 hit = 1;
         }
 
         //Calculate shortly distance to wall sprite for currnt ray
         if (hit_side)
-            ray.dist = (ray.cur_dist.x - ray.pos.x + (1 - step.x) / 2) * ray.ratio.x;
+            inf->ray.dist = (inf->ray.pos.x - inf->view.pos.x + (1 - step.x) / 2) / inf->ray.dir.x;
         else
-            ray.dist = (ray.cur_dist.y - ray.pos.y + (1 - step.y) / 2) * ray.ratio.y;
+            inf->ray.dist = (inf->ray.pos.y - inf->view.pos.y + (1 - step.y) / 2) / inf->ray.dir.y;
         
         //Calculate height of wall stripe for current ray
         int height;
-        height = (int)(HEIGHT / ray.dist);
+        height = (int)(HEIGHT / inf->ray.dist);
         
         //Define pixels of window for color filling 
         int draw_start;
         int draw_end;
         if ((draw_start = -height / 2 + HEIGHT / 2) < 0)
             draw_start = 0;
-        if ((draw_start = height / 2 + HEIGHT / 2) > HEIGHT - 1)
-            draw_start = HEIGHT - 1;
-        
-        //Draw in window
-        while (draw_start < draw_end + 1)
+        if ((draw_end = height / 2 + HEIGHT / 2) > HEIGHT)
+            draw_end = HEIGHT - 1;
+
+        // printf("x=%d\n", x_on_camera_plane);
+        // printf("dist=%.3f\n", ray.dist);
+        // printf("height=%d\n", height);
+        // printf("start=%d\n", draw_start);
+        // printf("end  =%d\n\n", draw_end);
+
+        //Draw in image
+        //draw floor
+        unsigned int color = 0x0059CAD5;
+        int ceil = 0;
+        while (ceil <= draw_start)
         {
-            mlx_pixel_put(mlx_ptr, win_ptr, x_on_camera_plane, draw_start, 0123123123);
+            my_mlx_pixel_put(&inf->img, x_on_camera_plane, ceil, color);
+            ceil++;
+        }
+        //draw wall
+        color = 0x00FF0000;
+        while (draw_start <= draw_end)
+        {
+            if (!hit_side)
+                color = 0x00AA0000;
+            my_mlx_pixel_put(&inf->img, x_on_camera_plane, draw_start, color);
             draw_start++;
         }
-
+        //draw floor
+        color = 0x55555555;
+        while (draw_start <= HEIGHT)
+        {
+            my_mlx_pixel_put(&inf->img, x_on_camera_plane, draw_start, color);
+            draw_start++;
+        }
+        
         //iter to next ray or to next camera plane point
         x_on_camera_plane++;
     }
+    return (0);
+}
+
+int **fill_map(int map[MAP_HEIGHT][MAP_WIDTH])
+{
+    int **new_map;
+
+    new_map = (int **)malloc(sizeof(int *) * MAP_HEIGHT);
+    for (int i = 0; i < MAP_HEIGHT; i++)
+    {
+        new_map[i] = (int *)malloc(sizeof(int) * MAP_WIDTH);
+        for (int j = 0; j < MAP_WIDTH; j++)
+            new_map[i][j] = map[i][j];        
+    }
+    return (new_map);
+}
+
+void clear_map(int **map, int height)
+{
+    int i;
+
+    i = 0;
+    while (i < height)
+    {
+        free(map[i]);
+        i++;
+    }
+    free(map);
+}
+
+void move_for_back(t_inf *inf, double dir)
+{
+    inf->view.pos.x += (inf->view.dir.x * MOVE_SPEED) * dir;
+    if (inf->map[(int)inf->view.pos.y][(int)inf->view.pos.x])
+        inf->view.pos.x -= (inf->view.dir.x * MOVE_SPEED) * dir;
+    inf->view.pos.y += (inf->view.dir.y * MOVE_SPEED) * dir;
+    if (inf->map[(int)inf->view.pos.y][(int)inf->view.pos.x])
+        inf->view.pos.y -= (inf->view.dir.y * MOVE_SPEED) * dir;
+}
+
+void move_left_right(t_inf *inf, double dir)
+{
+    inf->view.pos.x -= (inf->view.dir.y * MOVE_SPEED) * dir;
+    if (inf->map[(int)inf->view.pos.y][(int)inf->view.pos.x])
+        inf->view.pos.x += (inf->view.dir.y * MOVE_SPEED) * dir;
+    inf->view.pos.y += (inf->view.dir.x * MOVE_SPEED) * dir;
+    if (inf->map[(int)inf->view.pos.y][(int)inf->view.pos.x])
+        inf->view.pos.y -= (inf->view.dir.x * MOVE_SPEED) * dir;
+}
+
+void turn_left_right(t_inf *inf, double turn_sin, double turn_cos)
+{
+    double old_x;
+
+    old_x = inf->view.dir.x;
+    inf->view.dir.x =   inf->view.dir.x * turn_cos -
+                        inf->view.dir.y * turn_sin;
+    inf->view.dir.y =   inf->view.dir.y * turn_cos +
+                        old_x * turn_sin;
+    old_x = inf->view.cam_plane.x;
+    inf->view.cam_plane.x = inf->view.cam_plane.x * turn_cos -
+                            inf->view.cam_plane.y * turn_sin;
+    inf->view.cam_plane.y = inf->view.cam_plane.y * turn_cos +
+                            old_x * turn_sin;
+}
+
+int close_game(t_inf *inf)
+{
+    clear_map(inf->map, MAP_HEIGHT);
+    mlx_destroy_window(inf->mlx_ptr, inf->win_ptr);
+    free(inf->mlx_ptr);
+    free(inf);
+    exit (1);
+    return (0);
+}
+
+int event_nadling(t_inf *inf)
+{
+    if (inf->event.move_forawrd == 1)
+        move_for_back(inf, 1.0);
+    else if (inf->event.move_backword == 1)
+        move_for_back(inf, -1.0);
+    else if (inf->event.move_left == 1)
+        move_left_right(inf, 1.0);
+    else if (inf->event.move_right == 1)
+        move_left_right(inf, -1.0);
+    else if (inf->event.turn_left == 1)
+        turn_left_right(inf, inf->turn.sin_pozotive, inf->turn.cos_pozitive);
+    else if (inf->event.turn_right == 1)
+        turn_left_right(inf, inf->turn.sin_negative, inf->turn.cos_negative);
+    raycast(inf);
+    mlx_put_image_to_window(inf->mlx_ptr, inf->win_ptr, inf->img.img_ptr, 0 , 0);
+    return (0);
+}
+
+int key_release(int event_key, t_inf *inf)
+{
+    if (event_key == MOVE_FORWARD)
+       inf->event.move_forawrd = 0;
+    else if (event_key == MOVE_BACKWARD)
+       inf->event.move_backword = 0;
+    else if (event_key == MOVE_LEFT)
+        inf->event.move_left = 0;
+    else if (event_key == MOVE_RIGHT)
+        inf->event.move_right = 0;
+    else if (event_key == TURN_LEFT)
+        inf->event.turn_left = 0;
+    else if (event_key == TURN_RIGHT)
+        inf->event.turn_right = 0;
+    return (0);
+}
+
+int key_press(int event_key, t_inf *inf)
+{
+    if (event_key == MOVE_FORWARD)
+       inf->event.move_forawrd = 1;
+    else if (event_key == MOVE_BACKWARD)
+       inf->event.move_backword = 1;
+    else if (event_key == MOVE_LEFT)
+        inf->event.move_left = 1;
+    else if (event_key == MOVE_RIGHT)
+        inf->event.move_right = 1;
+    else if (event_key == TURN_LEFT)
+        inf->event.turn_left = 1;
+    else if (event_key == TURN_RIGHT)
+        inf->event.turn_right = 1;
+    else if (event_key == EXIT)
+        close_game(inf);
+    return (0);
+}
+
+int main(void)
+{
+    //Init cub3d
+    t_inf *inf;
+    if (!(inf = (t_inf *)malloc(sizeof(t_inf))))
+        return (0);
+
+    inf->mlx_ptr = mlx_init();
+    inf->win_ptr = mlx_new_window(inf->mlx_ptr, WIDTH, HEIGHT, "Test");
+
+    int	map[MAP_HEIGHT][MAP_WIDTH] =
+    {
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 1, 1},
+        {1, 1, 0, 0, 0, 0, 0, 1, 0, 1},
+        {1, 1, 1, 1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 0, 0, 0, 1, 0, 0, 0, 1},
+        {1, 1, 1, 0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    };
+
+    inf->map = fill_map(map);
+
+    t_view view;
+    view.dir.x = -1;
+    view.dir.y = 0.0;
+    view.pos.x = 8.9;
+    view.pos.y = 8.9;
+    view.cam_plane.x = 0;
+    view.cam_plane.y = 0.66;
+
+    inf->view = view;
+
+    inf->turn.cos_negative = cos(-TURN_SPEED);
+    inf->turn.cos_pozitive = cos(TURN_SPEED);
+    inf->turn.sin_negative = sin(-TURN_SPEED);
+    inf->turn.sin_pozotive = sin(TURN_SPEED);
+
+    inf->event.move_backword = 0;
+    inf->event.move_forawrd = 0;
+    inf->event.move_left = 0;
+    inf->event.move_right = 0;
+    inf->event.turn_left = 0;
+    inf->event.turn_right = 0;
+
+    //Raycast, draw and events handling
+    raycast(inf);
+    mlx_put_image_to_window(inf->mlx_ptr, inf->win_ptr, inf->img.img_ptr, 0 , 0);
+    
+	mlx_hook(inf->win_ptr, 2, 1L << 0, key_press, inf);
+	mlx_hook(inf->win_ptr, 3, 1L << 1, key_release, inf);
+    mlx_loop_hook(inf->mlx_ptr, event_nadling, inf);
+	mlx_hook(inf->win_ptr, 17, 1L << 17, close_game, inf);
+    mlx_loop(inf->mlx_ptr);
+
+    //Destroy game
+    close_game(inf);
     return 0;
 }
+
+// gcc -Werror -Wextra -Wall test.c -L minilibx/ -lmlx -lm -lbsd -lX11 -lXext
